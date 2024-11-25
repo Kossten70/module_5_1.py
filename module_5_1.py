265a366f86b7ae45bd4e7b7157fb3d174c3077bc
Konstantin <ref6512722@gmail.com>
2024-11-25 21:14:19 +0300

    002

--- module_5_1.py
+++ module_5_1.py
@@ -1,3 +1,17 @@
 class House:
-    def __init__(self, name, number_of_floors): #Название, кол-во этажей
+    def __init__(self, name, number_of_floors): #Название, кол-во этажей.
+        self.name = name
+        self.number_of_floors = number_of_floors
 
+
+
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+# h1.go_to(5)
+# h2.go_to(10)
+
+print(h1)
+print(h2)
